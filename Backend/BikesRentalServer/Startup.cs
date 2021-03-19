a90878271e8a4ca5dda781ab7217701fbd1f9e41
Marcin <marcinggalinski@gmail.com>
2021-03-20 00:37:28 +0100

    automap controllers to endpoints

--- Backend/BikesRentalServer/Startup.cs
+++ Backend/BikesRentalServer/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BikesRentalServer
@@ -16,6 +17,11 @@ namespace BikesRentalServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +36,7 @@ namespace BikesRentalServer
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
