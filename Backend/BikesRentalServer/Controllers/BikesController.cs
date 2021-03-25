92d9c469f03ef2a0617197384980751939ca9365
Potomski Tomasz <01142203@pw.edu.pl>
2021-03-25 14:28:06 +0100

    Added BikesController, BikesService, GetBikeResponse

---
+++ Backend/BikesRentalServer/Controllers/BikesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikesRentalServer.Services.Abstract;
+using BikesRentalServer.Dtos.Responses;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BikesRentalServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BikesController : ControllerBase
+    {
+        private readonly IBikesService bikesService;
+
+        public BikesController(IBikesService stationsService)
+        {
+            this.bikesService = stationsService;
+        }
+
+        // GET: api/<BikesController>
+        [HttpGet]
+        public ActionResult<IEnumerable<GetBikeResponse>> GetAllStations()
+        {
+            return Ok(bikesService.GetAllBikes()
+                .Select(bike => new GetBikeResponse
+                {
+                    id = bike.Id.ToString(),
+                    station = null,
+                    user = null
+                }));
+        }
+
+        // GET api/<BikesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
---
+++ Backend/BikesRentalServer/Services/BikesService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikesRentalServer.Services.Abstract;
+using BikesRentalServer.Controllers;
+namespace BikesRentalServer.Services
+{
+    public class BikesService : IBikesService
+    {
+        
+        public IEnumerable<Bike> GetAllBikes()
+        {
+            return new List<Bike>{
+                new Bike(){Id=1, Description="Description", State = BikeState.Working, Station = null, User = null }
+            };
+        }
+
+        public Bike GetBike(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
