3b12f840a26753f47387a2d16e5d8c96d366a52c
Piotr Duperas <piotrduperas@gmail.com>
2021-03-26 03:00:29 +0100

    Bikes & stations mocks

---
+++ Frontend/src/common/mocks/stations.ts
@@ -0,0 +1,28 @@
+import { Station } from './../api/models/station';
+
+export const mockedStations: Station[] = [
+  {
+    id: '1',
+    name: 'Górczewska 100',
+  },
+  {
+    id: '2',
+    name: 'Puławska 23C',
+  },
+  {
+    id: '3',
+    name: 'Aleje Jerozolimskie 126',
+  },
+  {
+    id: '4',
+    name: 'Towarowa 78',
+  },
+  {
+    id: '5',
+    name: 'Koszykowa 44',
+  },
+  {
+    id: '6',
+    name: 'Rondo Daszyńskiego',
+  },
+];
