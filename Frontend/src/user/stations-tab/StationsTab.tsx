ca2c5144f218d9f63650694108811892b4398b4c
Piotr Duperas <piotrduperas@gmail.com>
2021-03-26 03:00:41 +0100

    Displaying many stations

--- Frontend/src/user/stations-tab/StationsTab.tsx
+++ Frontend/src/user/stations-tab/StationsTab.tsx
@@ -11,6 +11,10 @@ import {
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
 import BikeTile from './BikeTile';
+import useMockedDataFetching from './../../common/mocks/useMockedDataFetching';
+import { mockedStations } from './../../common/mocks/stations';
+import Alert from '../../common/components/Alert';
+import Spinner from '../../common/components/Spinner';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,24 +25,34 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+const useDataFetching = useMockedDataFetching(mockedStations);
+
 const StationsTab = (): JSX.Element => {
   const classes = useStyles();
+  const { results: stations, error, loading } = useDataFetching('/stations');
 
   return (
-    <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography className={classes.heading}>Ul. Górczewska 100</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Grid container justify="center" spacing={2}>
-          {[0, 1, 2].map((value) => (
-            <Grid key={value} item>
-              <BikeTile id={value * 100} />
-            </Grid>
-          ))}
-        </Grid>
-      </AccordionDetails>
-    </Accordion>
+    <>
+      {loading && <Spinner />}
+      {error && <Alert severity="error">Oops! You are offline...</Alert>}
+      {stations &&
+        stations.map(({ name, id }) => (
+          <Accordion key={id}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography className={classes.heading}>{name}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container justify="center" spacing={2}>
+                {[0, 1, 2].map((value) => (
+                  <Grid key={value} item>
+                    <BikeTile id={value * 100} />
+                  </Grid>
+                ))}
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+    </>
   );
 };
 
