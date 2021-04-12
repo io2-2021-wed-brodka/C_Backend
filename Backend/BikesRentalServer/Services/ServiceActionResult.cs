e84cb890060e3e8661e224b1f603daf61e6a2d83
Piotr Duperas <piotrduperas@gmail.com>
2021-04-12 20:03:44 +0200

    More static

--- Backend/BikesRentalServer/Services/ServiceActionResult.cs
+++ Backend/BikesRentalServer/Services/ServiceActionResult.cs
@@ -2,9 +2,36 @@
 {
     public class ServiceActionResult<T>
     {
-        public string Message { get; set; }
-        public T Object { get; set; }
-        public Status Status { get; set; }
+        public string Message { get; private set; }
+        public T Object { get; private set; }
+        public Status Status { get; private set; }
+
+        public static ServiceActionResult<T> Success(T result)
+        {
+            return new ServiceActionResult<T>
+            {
+                Object = result,
+                Status = Status.Success
+            };
+        }
+
+        public static ServiceActionResult<T> EntityNotFound(string message)
+        {
+            return new ServiceActionResult<T>
+            {
+                Status = Status.EntityNotFound,
+                Message = message
+            };
+        }
+
+        public static ServiceActionResult<T> InvalidState(string message)
+        {
+            return new ServiceActionResult<T>
+            {
+                Status = Status.InvalidState,
+                Message = message
+            };
+        }
     }
 
     public enum Status
