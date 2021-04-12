ef1381f10d49b1b61bd9b60a7dd39ed622eb08dc
Piotr Duperas <piotrduperas@gmail.com>
2021-04-12 21:40:21 +0200

    Admin app layout

--- Frontend/src/admin/AdminApp.tsx
+++ Frontend/src/admin/AdminApp.tsx
@@ -1,8 +1,61 @@
 import React from 'react';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  ThemeProvider,
+} from '@material-ui/core';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { services, ServicesContext } from '../common/services';
+import LoginPage from '../common/components/LoginPage';
+import { getTokenFromLocalStorage } from '../common/authentication/token-functions';
+import pinkTheme from '../common/theme';
+import Layout from './Layout';
+
 import './AdminApp.css';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    rootMobile: {
+      flexGrow: 1,
+      background: theme.palette.grey[100],
+      height: 'auto',
+      overflow: 'auto',
+    },
+  }),
+);
+
 const AdminApp = () => {
-  return <div>Admin app</div>;
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={pinkTheme}>
+      <ServicesContext.Provider value={services}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login">
+              <div className={classes.rootMobile}>
+                <LoginPage />
+              </div>
+            </Route>
+            <Route exact path="/">
+              <Redirect to={'/stations'} />
+            </Route>
+            <Route
+              path="/"
+              render={() => (
+                <>
+                  {!getTokenFromLocalStorage() && <Redirect to={'/login'} />}
+                  <Layout>
+                    <h1>Admin will be here, stay tuned</h1>
+                  </Layout>
+                </>
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
+      </ServicesContext.Provider>
+    </ThemeProvider>
+  );
 };
 
 export default AdminApp;
---
+++ Frontend/src/admin/Layout.tsx
@@ -0,0 +1,207 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import PeopleIcon from '@material-ui/icons/People';
+import RoomIcon from '@material-ui/icons/Room';
+import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
+import BuildIcon from '@material-ui/icons/Build';
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Team C'}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+};
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    width: '100%',
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+}));
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden,
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            CityBikes Admin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <div>
+            <ListItem button>
+              <ListItemIcon>
+                <RoomIcon />
+              </ListItemIcon>
+              <ListItemText primary="Stations" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <DirectionsBikeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Bikes" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Users" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <BuildIcon />
+              </ListItemIcon>
+              <ListItemText primary="Techs" />
+            </ListItem>
+          </div>
+        </List>
+        <Divider />
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Paper className={classes.paper}>{children}</Paper>
+          <Box pt={4}>
+            <Copyright />
+          </Box>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
