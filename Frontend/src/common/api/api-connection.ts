44a46a90820d2d97e4f64bd70a07fb6966e2e269
Piotr Duperas <piotrduperas@gmail.com>
2021-04-13 18:57:42 +0200

    Reading error messages from backend

--- Frontend/src/common/api/api-connection.ts
+++ Frontend/src/common/api/api-connection.ts
@@ -3,9 +3,11 @@ const apiConnection = <T>(
   url: string,
   params?: AxiosRequestConfig,
 ): Promise<T> => {
-  return axios({ ...params, url }).then(
-    response => response.data as T,
-  ) as Promise<T>;
+  return axios({ ...params, url })
+    .then(response => response.data as T)
+    .catch(err => {
+      throw new Error(err?.response?.data?.message || err.message);
+    }) as Promise<T>;
 };
 
 export default apiConnection;
