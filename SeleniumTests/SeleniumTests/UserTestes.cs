fcff6530d8e0884bb17e4ba50bdaa0b6cde750ad
Potomski Tomasz <01142203@pw.edu.pl>
2021-04-13 14:14:10 +0200

    Selenium tests init

---
+++ SeleniumTests/SeleniumTests/UserTestes.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SeleniumTests
+{
+    public class UserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
