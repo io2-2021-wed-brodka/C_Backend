6fbc7355f784a93fdfb1e6a6c5153ba0cf2d934e
Potomski Tomasz <01142203@pw.edu.pl>
2021-04-28 04:17:16 +0200

    Added login test for admin

---
+++ Backend/SeleniumTests/SeleniumTests/AdminTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTests
+{
+    class AdminTests
+    {
+        readonly string _login = "admin";
+        readonly string _password = "admin";
+        readonly string _url = "http://localhost:3002";
+        FirefoxOptions firefoxOptions;
+        private void Login(IWebDriver webDriver)
+        {
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(_url);
+            webDriver.FindElement(By.Id("login")).SendKeys(_login);
+            webDriver.FindElement(By.Id("password")).SendKeys(_password);
+            webDriver.FindElement(By.CssSelector("button")).Click();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            firefoxOptions = new FirefoxOptions();
+            firefoxOptions.AcceptInsecureCertificates = true;
+        }
+
+        [Test]
+        public void VerifyLoginAsValidUser()
+        {
+            IWebDriver webDriver = new FirefoxDriver(firefoxOptions);
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(_url);
+            webDriver.FindElement(By.Id("login")).SendKeys(_login);
+            webDriver.FindElement(By.Id("password")).SendKeys(_password);
+            webDriver.FindElement(By.CssSelector("button")).Click();
+            Assert.AreEqual(webDriver.Url, _url + "/stations");
+            webDriver.Close();
+        }
+
+
+
+    }
+}
