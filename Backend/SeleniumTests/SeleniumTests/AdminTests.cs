8bdc20b65a12f0785c3fede0877d169b1353496d
Potomski Tomasz <01142203@pw.edu.pl>
2021-04-28 09:46:03 +0200

    Added remove/add bikes tests remove/add station test

--- Backend/SeleniumTests/SeleniumTests/AdminTests.cs
+++ Backend/SeleniumTests/SeleniumTests/AdminTests.cs
@@ -4,7 +4,7 @@ using OpenQA.Selenium.Firefox;
 
 namespace SeleniumTests
 {
-    class AdminTests
+    public class AdminTests
     {
         readonly string _login = "admin";
         readonly string _password = "admin";
@@ -83,6 +83,105 @@ namespace SeleniumTests
             webDriver.Close();
         }
 
+        [Test]
+        public void VerifyAddingStation()
+        {
+            IWebDriver webDriver = new FirefoxDriver(firefoxOptions);
+            Login(webDriver);
+            var stationsCount = webDriver.FindElements(By.CssSelector(".MuiPaper-root.MuiAccordion-root.MuiAccordion-rounded.MuiPaper-elevation1.MuiPaper-rounded")).Count;
+            webDriver.FindElement(By.CssSelector("input")).SendKeys("stacja selenium");
+            var buttons = webDriver.FindElements(By.CssSelector("button"));
+            foreach(var button in buttons)
+            {
+                if(button.Text == "ADD STATION")
+                {
+                    button.Click();
+                    break;
+                }
+            }
+            int newStationCount = webDriver.FindElements(By.CssSelector(".MuiPaper-root.MuiAccordion-root.MuiAccordion-rounded.MuiPaper-elevation1.MuiPaper-rounded")).Count;
+            Assert.AreEqual(stationsCount + 1, newStationCount);
 
+            webDriver.Close();
+        }
+
+        [Test]
+        public void VerifyAddingBikeToStation()
+        {
+            IWebDriver webDriver = new FirefoxDriver(firefoxOptions);
+            Login(webDriver);
+            var station = webDriver.FindElement(By.CssSelector("#root > main.makeStyles-content-12 > div.MuiContainer-maxWidthLg.MuiContainer-root.makeStyles-container-13 > div.MuiAccordion-root.MuiAccordion-rounded.MuiPaper-elevation1.MuiPaper-root.MuiPaper-rounded:nth-child(5) > div.MuiAccordionSummary-root.MuiButtonBase-root"));
+            station.Click();
+            int bikesCount = webDriver.FindElements(By.CssSelector("li")).Count;
+            var buttons = webDriver.FindElements(By.CssSelector("button:nth-child(1)"));
+            foreach (var button in buttons)
+            {
+                if(button.Text == "ADD BIKE")
+                {
+                    button.Click();
+                    break;
+                }
+            }
+            int newBikesCount = webDriver.FindElements(By.CssSelector("li")).Count;
+            Assert.AreEqual(bikesCount + 1, newBikesCount);
+            webDriver.Close();
+        }
+
+        [Test]
+        public void VerifyRemoveStation()
+        {
+            IWebDriver webDriver = new FirefoxDriver(firefoxOptions);
+            Login(webDriver);
+            var stations = webDriver.FindElements(By.CssSelector(".MuiPaper-root.MuiAccordion-root.MuiAccordion-rounded.MuiPaper-elevation1.MuiPaper-rounded"));
+            int stationsCount = stations.Count;
+            var station = stations[stations.Count-1];
+            station.Click();
+            var buttons = webDriver.FindElements(By.CssSelector("button"));
+            foreach (var button in buttons)
+            {
+                if(button.Text == "REMOVE")
+                {
+                    button.Click();
+                    break;
+                }
+            }
+            stations = webDriver.FindElements(By.CssSelector(".MuiPaper-root.MuiAccordion-root.MuiAccordion-rounded.MuiPaper-elevation1.MuiPaper-rounded"));
+            int newStationsCount = stations.Count;
+            Assert.AreEqual(stationsCount, newStationsCount + 1);
+        }
+
+        [Test]
+        public void VerifyRemovingBikeFromStation()
+        {
+            IWebDriver webDriver = new FirefoxDriver(firefoxOptions);
+            Login(webDriver);
+            var station = webDriver.FindElement(By.CssSelector("#root > main.makeStyles-content-12 > div.MuiContainer-maxWidthLg.MuiContainer-root.makeStyles-container-13 > div.MuiAccordion-root.MuiAccordion-rounded.MuiPaper-elevation1.MuiPaper-root.MuiPaper-rounded:nth-child(5) > div.MuiAccordionSummary-root.MuiButtonBase-root"));
+            station.Click();
+            int bikesCount = webDriver.FindElements(By.CssSelector("li")).Count;
+            var list = webDriver.FindElement(By.CssSelector("ul"));
+            var blockButtons = webDriver.FindElements(By.CssSelector("li  button:nth-child(1)"));
+            foreach (var button in blockButtons)
+            {
+                if (button.Text == "BLOCK")
+                {
+                    button.Click();
+                    break;
+                }
+            }
+            var RemoveButtons = webDriver.FindElements(By.CssSelector("li button:nth-child(2)"));
+            {
+                foreach (var button in RemoveButtons)
+                {
+                    if (button.Text == "REMOVE")
+                    {
+                        button.Click();
+                        break;
+                    }
+                }
+            }
+            int newBikesCount = webDriver.FindElements(By.CssSelector("li")).Count;
+            Assert.AreEqual(bikesCount, newBikesCount + 1);
+            webDriver.Close();
+        }
     }
 }
