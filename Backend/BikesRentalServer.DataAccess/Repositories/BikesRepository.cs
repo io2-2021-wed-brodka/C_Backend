20fd265ad1a7574f5528c1923eef164fb07a0abf
Marcin Galiński <marcing305@wp.pl>
2021-05-04 17:05:10 +0200

    add BikesRepository.Associate tests + fix BikesRepository.Associate

--- Backend/BikesRentalServer.DataAccess/Repositories/BikesRepository.cs
+++ Backend/BikesRentalServer.DataAccess/Repositories/BikesRepository.cs
@@ -78,7 +78,7 @@ namespace BikesRentalServer.DataAccess.Repositories
         public Bike Associate(string id, User user)
         {
             var bike = Get(id);
-            if (bike is null)
+            if (bike is null || !_dbContext.Users.Contains(user))
                 return null;
 
             bike.Station = null;
@@ -91,7 +91,7 @@ namespace BikesRentalServer.DataAccess.Repositories
         public Bike Associate(string id, Station station)
         {
             var bike = Get(id);
-            if (bike is null)
+            if (bike is null || !_dbContext.Stations.Contains(station))
                 return null;
 
             bike.User = null;
