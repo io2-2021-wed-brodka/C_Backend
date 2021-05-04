5386cbee874669b32fb3103edbd50cb80660902c
Marcin Galiński <marcing305@wp.pl>
2021-05-04 18:21:02 +0200

    add UsersRepository.Get tests

---
+++ Backend/BikesRentalServer.Tests/RepositoriesTests/UsersRepositoryTests/Get.cs
@@ -0,0 +1,58 @@
+﻿using BikesRentalServer.DataAccess;
+using BikesRentalServer.DataAccess.Repositories;
+using BikesRentalServer.Models;
+using BikesRentalServer.Tests.Mock;
+using FluentAssertions;
+using Xunit;
+
+namespace BikesRentalServer.Tests.RepositoriesTests.UsersRepositoryTests
+{
+    public class Get
+    {
+        private readonly DatabaseContext _dbContext;
+        private readonly UsersRepository _usersRepository;
+        
+        public Get()
+        {
+            _dbContext = MockedDbFactory.GetContext();
+            _usersRepository = new UsersRepository(_dbContext);
+        }
+
+        [Fact]
+        public void GetShouldReturnRequestedUserForValidId()
+        {
+            const int id = 2;
+            var user = _dbContext.Users.Add(new User
+                {
+                    Username = "user",
+                    Role = UserRole.User,
+                })
+                .Entity;
+            _dbContext.SaveChanges();
+
+            var result = _usersRepository.Get(id.ToString());
+            
+            result.Should().BeEquivalentTo(user);
+        }
+
+        [Fact]
+        public void GetShouldReturnNullIfNoUsersWithProvidedId()
+        {
+            const string id = "1";
+
+            var result = _usersRepository.Get(id);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetShouldReturnNullForInvalidId()
+        {
+            const string id = "invalid id";
+
+            var result = _usersRepository.Get(id);
+
+            result.Should().BeNull();
+        }
+    }
+}
