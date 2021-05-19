62076545f7ab61c7c7848532e4a8787cadcb4c3d
Piotr Duperas <piotrduperas@gmail.com>
2021-05-19 08:27:54 +0200

    Added GetBlockedUsers to repository

--- Backend/BikesRentalServer.DataAccess/Repositories/Abstract/IUsersRepository.cs
+++ Backend/BikesRentalServer.DataAccess/Repositories/Abstract/IUsersRepository.cs
@@ -1,9 +1,11 @@
 ﻿using BikesRentalServer.Models;
+using System.Collections.Generic;
 
 namespace BikesRentalServer.DataAccess.Repositories.Abstract
 {
     public interface IUsersRepository : IRepository<User>
     {
+        IEnumerable<User> GetBlockedUsers();
         User GetByUsername(string username);
         User GetByUsernameAndPassword(string username, string password);
         User SetStatus(string id, UserStatus status);
