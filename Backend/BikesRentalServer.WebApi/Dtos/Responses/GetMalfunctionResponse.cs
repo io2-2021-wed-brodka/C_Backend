b41f4b87471c2196d45da3c2a854d59c308d7c0f
Potomski Tomasz <01142203@pw.edu.pl>
2021-05-23 18:14:32 +0200

    Added MalfunctionsController and dto classes

---
+++ Backend/BikesRentalServer.WebApi/Dtos/Responses/GetMalfunctionResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikesRentalServer.WebApi.Dtos.Responses
+{
+    public class GetMalfunctionResponse
+    {
+        public string Id { get; set; }
+        public string BikeId { get; set; }
+        public string Description { get; set; }
+        public string ReportingUserId { get; set; } 
+    }
+}
