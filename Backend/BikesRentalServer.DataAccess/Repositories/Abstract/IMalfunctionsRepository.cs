edba164d29876c9ff74543550d6b1de66acef3c2
Potomski Tomasz <01142203@pw.edu.pl>
2021-05-23 18:12:11 +0200

    Added MalfunctionsRepository

---
+++ Backend/BikesRentalServer.DataAccess/Repositories/Abstract/IMalfunctionsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikesRentalServer.Models;
+
+namespace BikesRentalServer.DataAccess.Repositories.Abstract
+{
+    public interface IMalfunctionsRepository : IRepository<Malfunction>
+    {
+
+    }
+}
