7bbd4eb3459e2e7441d66b5335098446e66f469d
Piotr Duperas <piotrduperas@gmail.com>
2021-05-25 17:05:18 +0000

    Removed usings

--- Backend/SeleniumTests2/Tests/AdminBikeOperationsTests.cs
+++ Backend/SeleniumTests2/Tests/AdminBikeOperationsTests.cs
@@ -1,8 +1,4 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
