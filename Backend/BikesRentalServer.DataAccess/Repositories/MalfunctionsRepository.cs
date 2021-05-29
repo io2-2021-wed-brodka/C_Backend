34c398fda2541825d6498b3551e87fec9e77f4bf
aolesiakmini <apietruszka11@gmail.com>
2021-05-29 11:47:36 +0200

    Create malfunctions repo, interface and class with tests.

---
+++ Backend/BikesRentalServer.DataAccess/Repositories/MalfunctionsRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using BikesRentalServer.DataAccess.Repositories.Abstract;
+using BikesRentalServer.Models;
+
+namespace BikesRentalServer.DataAccess.Repositories
+{
+    public class MalfunctionsRepository : IMalfunctionsRepository
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public MalfunctionsRepository(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public IEnumerable<Malfunction> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Malfunction Get(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Malfunction Add(Malfunction entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Malfunction Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Backend/BikesRentalServer.Tests/RepositoriesTests/MalfunctionsRepositoryTests/Remove.cs
@@ -0,0 +1,19 @@
+﻿using BikesRentalServer.DataAccess;
+using BikesRentalServer.DataAccess.Repositories;
+using BikesRentalServer.DataAccess.Repositories.Abstract;
+using BikesRentalServer.Tests.Mock;
+
+namespace BikesRentalServer.Tests.RepositoriesTests.MalfunctionsRepositoryTests
+{
+    public class Remove
+    {
+        private readonly DatabaseContext _dbContext;
+        private readonly IMalfunctionsRepository _malfunctionsRepository;
+
+        public Remove()
+        {
+            _dbContext = MockedDbFactory.GetContext();
+            _malfunctionsRepository = new MalfunctionsRepository(_dbContext);
+        }
+    }
+}
