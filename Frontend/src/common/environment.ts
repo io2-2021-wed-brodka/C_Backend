a70236c367c2f80dbb81b7a9ab0685163f0b6f5f
Marcin Galiński <marcing305@wp.pl>
2021-06-09 00:12:03 +0200

    adjust backend path on frontend based on env

--- Frontend/src/common/environment.ts
+++ Frontend/src/common/environment.ts
@@ -1,3 +1,4 @@
 export const isAdminApp = () => process.env.REACT_APP_FOR === 'admin';
-
 export const isUserApp = () => process.env.REACT_APP_FOR === 'user';
+export const isDevelopment = () => process.env.NODE_ENV === 'development';
+
